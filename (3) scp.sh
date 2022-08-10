c72734719e1370e7869ff9e63fab1095fdc10f39
Isaiah Cai <46073235+IsaiahCai@users.noreply.github.com>
2022-08-10 11:12:35 +0100

    Delete (3) scp.sh

--- (3) scp.sh
+++
@@ -1,5 +0,0 @@
-# 
-for batch in '$path to the folders (containing cifs files)'/*
-do
-    expect expect.exp $batch '$destination path on high-performance computer' login.hpc.imperial.ac.uk '$account' '$password'
-done
